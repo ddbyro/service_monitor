c2649c2f10cd2e6040bcc831efaf35e2f945a75c
David Byron <ddbyro@gmail.com>
2020-11-02 23:28:23 -0700

    test

--- service-monitor.py
+++ service-monitor.py
@@ -65,5 +65,5 @@ def rev():
 
 
 
-# if __name__ == '__main__':
-#    app.run(host='0.0.0.0', port=5002, debug = True)
+if __name__ == '__main__':
+   app.run(host='0.0.0.0', port=5002, debug = True)

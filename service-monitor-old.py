31a3d7a719e857cc5fef23042a8e349733d174da
David Byron <ddbyro@gmail.com>
2020-11-03 09:03:20 -0700

    test

--- service-monitor-old.py
+++ service-monitor-old.py
@@ -61,7 +61,7 @@ def rev_mon(env):
                status_color = 'red'
        except:
            status_color = 'red'
-       print '{0}, {1}, {2}, {3}, {4}'.format(hostname['environment'], hostname['version'], status_color, test_mode, hostname['hostname']) #prints off information populated that will be used in the database update
+       print '{0}_servers, {1}, {2}, {3}, {4}'.format(hostname['environment'], hostname['version'], status_color, test_mode, hostname['hostname']) #prints off information populated that will be used in the database update
 
 
    #return render_template('rev.html',rows = rows)

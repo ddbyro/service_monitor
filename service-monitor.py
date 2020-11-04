e51660efc4dbf1ab6a7cbf508610b29bd386c391
David Byron <ddbyro@gmail.com>
2020-11-03 18:25:31 -0700

    testing

--- service-monitor.py
+++ service-monitor.py
@@ -1,34 +1,36 @@
+#!/usr/bin/env python
 from flask import Flask, render_template, request, redirect
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import yaml
 app = Flask(__name__)
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-import yaml
 
 config = yaml.full_load(open('./config.yaml'))
+hostnames = config['services']
 
-#connection information to database, and Rev
 def service_mon(env):
-   hostnames = []
-   host_info = [('{0}, {1}, {2}, {3}, {4}'.format(hostnames, version, status_color, test_mode, hostname))]
-   #generate list of hostnames
-   for hostname in config['services']:
-       if hostname['environment'] == "prod":
-           hostnames.append(hostname['hostname'])
+   service_info = []
 
+   for hostname in config['services']:
+       #test_mode = "Status: {0}".format('false')
        try:
-           # this is for the isAlive check column for all three brands
            service_isalive = requests.get('{0}'.format(hostname['hostname']), timeout=5)
-           if service_isalive.text == 'true':
+
+           if service_isalive.status_code == requests.codes.ok:
                status_color = 'green'
+               service_info.append({'hostname':hostname['hostname'],'environment':hostname['environment'],'version':hostname['version'],'status_color': status_color})
            else:
                status_color = 'red'
+               service_info.append({'hostname':hostname['hostname'],'environment':hostname['environment'],'version':hostname['version'],'status_color': status_color})
        except:
            status_color = 'red'
+           service_info.update({'hostname':hostname['hostname'],'environment':hostname['environment'],'version':hostname['version'],'status_color': status_color})
+           #print '{0}_servers, {1}, {2}, {3}, {4}'.format(service_info['host_status']['environment'], service_info['host_status']['version'], service_info['host_status']['status_color'], test_mode, service_info['host_status']['hostname']) #prints off information populated that will be used in the database update
+   #return service_info
+   return render_template('service01.html',service_info = service_info)
 
-
-   return render_template('service01.html',hostnames)
-
+#print(service_mon('prod'))
 
 @app.route('/')
 def home():
@@ -40,24 +42,25 @@ def rev():
     return service_mon('prod')
 
 
-# @app.route('/rev_dev')
-# def rev_dev():
-#    return rev_mon('dev')
+@app.route('/rev_dev')
+def rev_dev():
+   return rev_mon('dev')
 
 
-# @app.route('/rev_test')
-# def rev_test():
-#    return rev_mon('test')
+@app.route('/rev_test')
+def rev_test():
+   return rev_mon('test')
 
 
 
 
-# @app.route('/rev_stage')
-# def rev_stage():
-#    return rev_mon('stage')
+@app.route('/rev_stage')
+def rev_stage():
+   return rev_mon('stage')
 
 
 
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=5002, debug = True)
+

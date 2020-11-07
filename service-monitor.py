07d62f319ddfc61099e82e4eca2eb17edb958626
David Byron <ddbyro@gmail.com>
2020-11-06 20:27:20 -0700

    test

--- service-monitor.py
+++ service-monitor.py
@@ -28,7 +28,7 @@ def service_mon(env):
            service_info.append({'name':hostname['name'],'hostname':hostname['hostname'],'environment':hostname['environment'],'version':hostname['version'],'status_color': status_color})
            #print '{0}_servers, {1}, {2}, {3}, {4}'.format(service_info['host_status']['environment'], service_info['host_status']['version'], service_info['host_status']['status_color'], test_mode, service_info['host_status']['hostname']) #prints off information populated that will be used in the database update
    #return service_info
-   return render_template('service01.html',service_info = service_info)
+   return render_template('service_mon.html',service_info = service_info)
 
 #print(service_mon('prod'))
 
@@ -37,17 +37,17 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/service01')
+@app.route('/service_mon_prod')
 def service():
     return service_mon('prod')
 
 
-@app.route('/service01_dev')
+@app.route('/service_mon_dev')
 def service_dev():
    return service_mon('dev')
 
 
-@app.route('/service01_stage')
+@app.route('/service_mon_stage')
 def service_stage():
    return servic_mon('stage')
 

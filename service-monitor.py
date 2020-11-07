44b796f4bc9c3cdc8c89297e11b9d2b4b076d43a
David Byron <ddbyro@gmail.com>
2020-11-06 21:34:28 -0700

    test

--- service-monitor.py
+++ service-monitor.py
@@ -9,7 +9,7 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 config = yaml.full_load(open('./config/config.yaml'))
 hostnames = config['services']
 
-def service_mon(environment):
+def service_mon(service_env):
    service_info = []
 
    for hostname in config['services']:
@@ -26,9 +26,10 @@ def service_mon(environment):
        except:
            status_color = 'red'
            service_info.append({'name':hostname['name'],'hostname':hostname['hostname'],'environment':hostname['environment'],'version':hostname['version'],'status_color': status_color})
+           print(environment)
            #print '{0}_servers, {1}, {2}, {3}, {4}'.format(service_info['host_status']['environment'], service_info['host_status']['version'], service_info['host_status']['status_color'], test_mode, service_info['host_status']['hostname']) #prints off information populated that will be used in the database update
    #return service_info
-   return render_template('service_mon_{0}.html'.format('prod'),service_info = service_info)
+   return render_template('service_mon_{0}.html'.format(service_env),service_info = service_info)
 
 #print(service_mon('prod'))
 
